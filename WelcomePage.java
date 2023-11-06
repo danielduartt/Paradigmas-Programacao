5a769c7628acf7770187bac9bb90a3006d2b7e7d
Lucas Matos <lucasmmatos05@gmail.com>
2023-11-06 18:22:53 -0300

    Update feature - Login

--- WelcomePage.java
+++ WelcomePage.java
@@ -6,16 +6,21 @@ import javax.swing.JLabel;
 public class WelcomePage {
     
     JFrame frame = new JFrame();
-    JLabel welcomeLabel = new JLabel("Ola!");
+    JLabel welcomeTitle = new JLabel("Gerenciador de atividade da Escola JF");
+    JLabel welcomeSubtitle = new JLabel("Bem vindo");
 
     public WelcomePage(){
 
-        welcomeLabel.setBounds(170,170,200,35);
-        welcomeLabel.setFont(new Font(null , Font.PLAIN, 25));
+        welcomeTitle.setBounds(20,20,600,35);
+        welcomeTitle.setFont(new Font(null , Font.BOLD, 25));
 
-        frame.add(welcomeLabel);
+        welcomeSubtitle.setBounds(21,47,600,35);
+        welcomeSubtitle.setFont(new Font(null , Font.PLAIN, 18));
+
+        frame.add(welcomeSubtitle);
+        frame.add(welcomeTitle);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420 , 420);
+        frame.setSize(1900 , 1000);
         frame.setLayout(null);
         frame.setVisible(true);
 
